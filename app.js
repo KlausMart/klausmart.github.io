7a8a04eaeec1cc30f90fe405131b5ea01ffa4cdd
Claudio Martinich <claudio_martinich@hotmail.com>
2023-01-18 18:47:52 -0300

    Segunda version Proyecto Final

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+function crearCV(a,b,c,d,e) {
+  return a+b+c+d+e
+}
+
 function calcularEdad(a,b) {
   return a-b
 }
@@ -11,9 +15,10 @@ document.getElementById(`crear-cv`).addEventListener(`click`, function () {
   console.log ("El Email es: "+email)
   let direccion = document.getElementById(`direccion`).value;
   console.log ("La dirección es: "+direccion)
-  let fecha = document.getElementById(`direccion`).value;
+  let fecha = document.getElementById(`fecha`).value;
   console.log ("La fecha de nacimiento es: "+fecha)
-  let datos = crearCV("Nombre: "+nombre, "Teléfono: "+telefono, "Email: "+email, "Direción: "+direccion, "Fecha de Naciemitno: "+fecha);
+  let datos = crearCV("Nombre: "+nombre, ", ", "Teléfono: "+telefono, ", ", "Email: "+email, ", ",
+                       "Direción: "+direccion, ", ", "Fecha de Naciemitno: "+fecha);
   console.log ("Los datos de la persona son: "+datos)
 
   document.getElementById(`contenedor-nombre`).innerHTML = nombre
