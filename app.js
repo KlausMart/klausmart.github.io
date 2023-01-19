8d268836e8fd38c4373d7d6831e05fc109ebdf28
Claudio Martinich <claudio_martinich@hotmail.com>
2023-01-19 19:00:10 -0300

    tercer version Proyecto Final

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ function crearCV(a,b,c,d,e) {
   return a+b+c+d+e
 }
 
-function calcularEdad(a,b) {
-  return a-b
+function calcularEdad(fecha) {
+  let hoy = new Date()
+  let fechaNacimiento = new Date(fecha)
+  let edad = hoy.getFullYear() - fechaNacimiento.getFullYear()
+  let diferenciaMeses = hoy.getMonth() - fechaNacimiento.getMonth()
+  if (
+    diferenciaMeses < 0 ||
+    (diferenciaMeses === 0 && hoy.getDate() < fechaNacimiento.getDate())
+  ) {
+    edad--
+  }
+  return edad
 }
 
 document.getElementById(`crear-cv`).addEventListener(`click`, function () {
@@ -17,10 +27,14 @@ document.getElementById(`crear-cv`).addEventListener(`click`, function () {
   console.log ("La dirección es: "+direccion)
   let fecha = document.getElementById(`fecha`).value;
   console.log ("La fecha de nacimiento es: "+fecha)
-  let datos = crearCV("Nombre: "+nombre, ", ", "Teléfono: "+telefono, ", ", "Email: "+email, ", ",
-                       "Direción: "+direccion, ", ", "Fecha de Naciemitno: "+fecha);
+  let datos = crearCV("Nombre: "+nombre, ", ", "Teléfono: "+telefono, ", ", "Email: "+email, ", "
+  , "Direción: "+direccion, ", ", "Fecha de Naciemitno: "+fecha);
   console.log ("Los datos de la persona son: "+datos)
 
   document.getElementById(`contenedor-nombre`).innerHTML = nombre
+  document.getElementById(`contenedor-telefono`).innerHTML = telefono
+  document.getElementById(`contenedor-email`).innerHTML = email
+  document.getElementById(`contenedor-direccion`).innerHTML = nombre
+  document.getElementById(`contenedor-edad`).innerHTML = calcularEdad()
   
 });
