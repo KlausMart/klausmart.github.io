64a6c0c0418156a55052f157c317a1079789eda7
Claudio Martinich <claudio_martinich@hotmail.com>
2023-02-18 16:10:21 -0300

    18/2 version proyecto final

--- funcion.js
+++ funcion.js
@@ -2,8 +2,8 @@ function entrevista() {
     let text;
     let contacto = prompt("Dejanos tu número de teléfono o email", "Escribí aquí");
     switch(contacto) {
-      default:
+    default:
         text = "Excelente! En la brevedad me pondré en contacto contigo";
     }
     document.getElementById("demo").innerHTML = text;
-  }
+}
