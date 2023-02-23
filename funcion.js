ad16f13699a16de09d028e4faa005330835f4a23
KlausMart <122306983+KlausMart@users.noreply.github.com>
2023-02-23 16:55:42 -0300

    Add files via upload

---
+++ funcion.js
@@ -0,0 +1,9 @@
+function entrevista() {
+    let text;
+    let contacto = prompt("Dejanos tu número de teléfono o email", "Escribí aquí");
+    switch(contacto) {
+    default:
+        text = "Excelente! En la brevedad me pondré en contacto contigo";
+    }
+    document.getElementById("demo").innerHTML = text;
+}
